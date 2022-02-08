a3916feab1974a37512ab7ef82e1d7c8241fac7d
Daniel <dkburton0@gmail.com>
2022-02-08 19:24:42 +0000

    Added fonts and styling applied to nav/header

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,8 +1,14 @@
-import { useState } from "react";
-
+import { useState, ReactElement } from "react";
+// Font Awesome
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// Bootstrap
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+// Next
+import Image from "next/image";
+import Link from "next/link";
 
 const Navigation = () => {
   const [toggle, setToggle] = useState(false);
@@ -16,9 +22,44 @@ const Navigation = () => {
       <Container>
         <Row className="header__content">
           <Col xs="6" md="3">
-            Hello
+            <div className="header__logo">
+              <img src="/images/logo.png" alt="" title="" />
+            </div>
           </Col>
-          <Col md="6" className={`nav-primary ${toggle ? "active" : ""}`}></Col>
+          <Col md="6" className={`nav-primary ${toggle ? "active" : ""}`}>
+            <Container className="ps-2 ps-sm-3">
+              <ul>
+                <li>
+                  <Link href="/">
+                    <a>Home</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/pricing">
+                    <a>Pricing</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/bridal">
+                    <a>Bridal</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/courses">
+                    <a>Courses</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/contact">
+                    <a>Contact</a>
+                  </Link>
+                </li>
+              </ul>
+            </Container>
+            <Container className="nav-primary__social ps-2 ps-sm-3">
+              <p>Socials here:</p>
+            </Container>
+          </Col>
           <Col xs="6" md="3" className="nav-toggle">
             <div
               className={`nav-toggle__btn ${toggle ? "active" : ""}`}
