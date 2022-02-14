3a2f7db977deccfb48e50beb09717413211208f7
Daniel <dkburton0@gmail.com>
2022-02-14 18:20:58 +0000

    fix font issue

--- pages/_app.js
+++ pages/_app.js
@@ -12,13 +12,9 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Courier+Prime:wght@400;700&family=Poppins:wght@300;400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&family=Roboto+Mono:wght@300;400;500&display=swap"
           rel="stylesheet"
         />
       </Head>
