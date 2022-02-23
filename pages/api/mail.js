f57cdbaba8c20e02fa09ffc5d2b889c6ec35248b
Daniel <dkburton0@gmail.com>
2022-02-23 18:16:27 +0000

    Merge branch 'dev' of https://github.com/dan9b6/jo-and-darley into dev

---
+++ pages/api/mail.js
@@ -0,0 +1,25 @@
+const mail = require("@sendgrid/mail");
+
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default (req, res) => {
+  const body = JSON.parse(req.body);
+
+  const message = `
+  Name: ${body.firstName} ${body.firstName}\r\n
+  Email: ${body.email}\r\n
+  Contact Number: ${body.phone}\r\n
+  Message: ${body.message}
+  `;
+
+  const data = {
+    to: "danbh7296@gmail.com",
+    from: "danbh7296@gmail.com",
+    subject: "New Web Form Message",
+    text: message,
+    html: message.replace(/\r\n/g, "<br />"),
+  };
+
+  mail.send(data);
+  res.status(200).json({ status: "Ok" });
+};
--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,127 @@
 import React from "react";
+import Banner from "../components/banner";
+import Section from "../components/section";
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Contact = () => {
-  return <div>contact</div>;
+  async function handleOnSubmit(e) {
+    e.preventDefault();
+    const formData = {};
+    Array.from(e.currentTarget.elements).forEach((field) => {
+      if (!field.name) return;
+      formData[field.name] = field.value;
+    });
+    fetch("/api/mail", {
+      method: "post",
+      body: JSON.stringify(formData),
+    })
+      .then((res) => {
+        console.log("success");
+        return res.json();
+      })
+      .catch((error) => console.log("error"));
+  }
+
+  return (
+    <main>
+      <Banner title="Contact" />
+      <Section>
+        <div className="form">
+          <Row>
+            <Col xs="12">
+              <img src="/images/logo.png" alt="Logo" className="pb-5" />
+            </Col>
+            <Col sm="6" md="3">
+              <h4>Opening Hours:</h4>
+              <ul>
+                <li>Monday: 08:00 - 18:00</li>
+                <li>Monday: 08:00 - 18:00</li>
+                <li>Monday: 08:00 - 18:00</li>
+                <li>Monday: 08:00 - 18:00</li>
+                <li>Monday: 08:00 - 18:00</li>
+                <li>Monday: 08:00 - 18:00</li>
+                <li>Monday: 08:00 - 18:00</li>
+              </ul>
+            </Col>
+            <Col sm="6" md="3">
+              <h4>Opening Hours:</h4>
+              <ul>
+                <li>Monday: 08:00 - 18:00</li>
+                <li>Monday: 08:00 - 18:00</li>
+                <li>Monday: 08:00 - 18:00</li>
+                <li>Monday: 08:00 - 18:00</li>
+                <li>Monday: 08:00 - 18:00</li>
+                <li>Monday: 08:00 - 18:00</li>
+                <li>Monday: 08:00 - 18:00</li>
+              </ul>
+            </Col>
+            <Col xs="12" md="6">
+              <form
+                className="contact-form"
+                method="post"
+                onSubmit={handleOnSubmit}
+              >
+                <Row>
+                  <Col xs="12" md="6">
+                    <div className="contact-form__group">
+                      <input
+                        type="text"
+                        name="firstName"
+                        placeholder="First name..."
+                      />
+                    </div>
+                  </Col>
+                  <Col xs="12" md="6">
+                    <div className="contact-form__group">
+                      <input
+                        type="text"
+                        name="lastName"
+                        placeholder="Last name..."
+                      />
+                    </div>
+                  </Col>
+                  <Col xs="12" md="6">
+                    <div className="contact-form__group">
+                      <input
+                        type="text"
+                        name="email"
+                        placeholder="Email address"
+                      />
+                    </div>
+                  </Col>
+                  <Col xs="12" md="6">
+                    <div className="contact-form__group">
+                      <input
+                        type="text"
+                        name="phone"
+                        placeholder="Contact number"
+                      />
+                    </div>
+                  </Col>
+                  <Col xs="12">
+                    <div className="contact-form__group">
+                      <textarea
+                        type="text"
+                        name="message"
+                        placeholder="Message"
+                      />
+                    </div>
+                  </Col>
+                </Row>
+                <div className="contact-form__group">
+                  <button className="btn btn-book">
+                    <span>Submit</span>
+                  </button>
+                </div>
+              </form>
+            </Col>
+          </Row>
+        </div>
+      </Section>
+    </main>
+  );
 };
 
 export default Contact;
