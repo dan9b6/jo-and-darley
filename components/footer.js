18fa66826a99a63e57b2583f8bea801f67f0c314
Daniel <dkburton0@gmail.com>
2022-04-22 17:18:35 +0100

    Push to live environment

--- components/footer.js
+++ components/footer.js
@@ -124,7 +124,7 @@ const Footer = () => {
           <Link href="/privacy-policy">
             <a>Privacy Policy</a>
           </Link>{" "}
-          - Copyright 2022 - Dan Burton
+          - Copyright 2022 - <a href="mailto:danbh7296@gmail.com">Dan Burton</a>
         </p>
       </Container>
     </footer>
